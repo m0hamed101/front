0652e92b2ab22c9e32a16613d32a92562e09d538
m0hamed101 <94570570+m0hamed101@users.noreply.github.com>
2023-07-03 14:10:00 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
           <Route exact path='/' element={<Home api={api} />} />
           <Route path='/shop' element={<Shop api={api} />} />
           <Route path='/contact' element={<Contact />} />
-          <Route path='/:id' element={<Item />} />
+          <Route path='/:id' element={<Item api={api} />} />
           <Route path='/cart' element={<Cart />} />
 
           <Route path="/login" element={!user ? <Login /> : <Home api={api} />} />
